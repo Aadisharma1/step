3bda31bcb1cbb2ba79f0492bac5f1aef049804b1
Rajrupa Das <rajrupadas6@gmail.com>
2026-03-10 08:43:40 +0530

    Add PalindromeChecker and application
    Implement a palindrome checker using a stack.

---
+++ UC11.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("\\s+", "");
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop())
+                return false;
+        }
+
+        return true;
+    }
+}
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        if (checker.checkPalindrome(input))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
